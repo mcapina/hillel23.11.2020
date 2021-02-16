b7c116588a0ff0fc2ceede0b442bc1f10b1493f0
qonkee <77988595+qonkee@users.noreply.github.com>
2021-02-16 17:19:25 +0200

    Update javas.js

---
+++ homework13/javas.js
@@ -0,0 +1,23 @@
+'use strict'
+/*Task 1
+<script>
+var a = 21;
+var b = 4;
+var rest = a % b;
+if (rest != 0) {
+ alert('Делится со следующим остатком ' + rest);
+} else {
+ alert('Делится, без остатка: ' + (a / b));
+}
+</script>
+
+/*Task 2
+
+<script>
+for(var i =0;i<7;i++){
+                for(var a=0;a<i;a++){
+                    document.write('*');
+                }
+                document.write('<br/>');
+            }
+</script>
