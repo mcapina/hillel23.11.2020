07a370ccd09d693e35c353fc4572f6bb5f7005c6
PerNikolay <Perestyuk.kolya@gmail.com>
2021-03-07 18:39:25 +0200

    homework16

---
+++ homework16/Hw16.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+public class Hw16 {
+    public static void main(String[] args) {
+        /*System.out.println("Введите число 1, 2 или 3: ");
+        Scanner inputFigure = new Scanner (System.in);
+        int x = inputFigure.nextInt ();
+        if (x==1)
+            System.out.println ("вы ввели 1");
+        else if (x==2)
+            System.out.println ("вы ввели 2");
+        else if (x==3)
+            System.out.println ("вы ввели 3");
+        else if (x>3)
+            System.out.println("не то");
+        if (x<=0)
+            System.out.println("и это не то");*/
+
+
+        /*System.out.println("Введите число 1, 2 или 3: ");
+        Scanner inputFigure = new Scanner (System.in);
+        int x = inputFigure.nextInt ();
+        switch (x) {
+            case 1: System.out.println("вы ввели число 1");
+            break;
+            case 2: System.out.println("вы ввели число 2");
+            break;
+            case 3: System.out.println("вы ввели число 3");
+            break;
+            default: System.out.println("не то");
+        }*/
+
+ //Необходимо вывести на экран числа от 5 до 1.
+
+        /*System.out.println("5 4 3 2 1"); так можно было?*/
+
+        /*int [] a={1,2,3,4,5};
+        for ( int i = a.length-1; i >= 0; i--) {
+            System.out.print(" "+ a[i]);
+        }*/
+
+//Необходимо вывести на экран таблицу умножения на 3:
+
+     /*int a=3;
+     for (int i=1;i<11;i++){
+            System.out.println(a+ " * " + i + " = " + a * i);
+            System.out.print("");
+        }*/
+
+
+
+// Найти среднее значение суммы чисел от 1 до 100
+
+        /*double num = 1;
+        double b;
+        double c;
+        double sum = 0;
+        c = sum / 100;
+        while(num <= 100){
+            b = (num + num);
+            sum = sum + b / 2;
+            num++;
+            c = (sum / 100);
+        }
+        System.out.println("суммы чисел = " + sum);
+        System.out.println("среднее значение = " + c );*/
+
+
+    }
+}
+
+
+
+
+
+
